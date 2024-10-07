d45234b74c811182dd6f0231cb683774dff2b386
Victor Nieto Palacios <nietopalaciosvictor@gmail.com>
2024-10-07 20:26:57 +0200

    Main

--- cesar.py
+++ cesar.py
@@ -1,12 +1,4 @@
 import time
-import nltk
-from nltk.corpus import cess_esp
-# Verifica si el corpus ya está descargado
-try:
-    nltk.data.find('corpora/cess_esp.zip')
-except LookupError:
-    nltk.download('cess_esp', quiet=True)
-spanish_words = set(cess_esp.words())
 
 # Crea un array con todas las letras del alfabeto, incluyendo la ñ si asi se indica
 alfabeto = [chr(i) for i in range(97, 123)]
@@ -22,8 +14,6 @@ palabra = input("Introduce la palabra a descifrar: ")
 # Si quieres probar todos los desplazamientos, se recorre un bucle para cada desplazamiento y se va imprimiendo cada palabra
 if probar == "si":
     pruebas = ""
-    palabra_correcta = ""
-    indice_correcto = 0
     for i in range(1, 27):
         for letra in palabra:
             if letra in alfabeto:
@@ -32,14 +22,9 @@ if probar == "si":
                 pruebas += alfabeto[nuevo_indice] 
             else:
                 pruebas += letra
-        if pruebas in spanish_words:
-            indice_correcto = i
-            palabra_correcta = pruebas
         print(f"{i}. {pruebas}")
         time.sleep(0.25)
         pruebas = ""
-
-    print("\n Parece que la palabra correcta es " + palabra_correcta + " con un desplazamiento de " + str(indice_correcto))
     
 # Si quieres un desplazamiento especifico, se pide un numero y se imprime la palabra resultante con ese desplazamiento
 if probar =="no":
