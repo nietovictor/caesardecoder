1a7b913dc1fd2845c7b56c2f734097992a914080
David <david.fmartin@alumnos.upm.es>
2024-11-01 20:20:26 +0100

    Added a search in the RAE

--- cesar.py
+++ cesar.py
@@ -1,4 +1,15 @@
 import time
+from pyrae import dle
+
+print("Bienvenido a Caesar Decoder")
+print("hecho por nietovictor and dawfunes")
+time.sleep(0.5)
+
+# Antes de nada, debemos seleccionar si queremos hacer con una rotación o cada cuantas letras.
+# print("Antes de nada, debemos seleccionar si queremos hacer con una rotación o cada cuantas letras.")
+# alternante = input("¿Quieres que se aplique el decodificado a todas las letras? si/no")
+# if alternante == "no":
+#     cada_cuantas_letras = input("Cada cuantas letras quieres que se aplique el codigo?")
 
 # Crea un array con todas las letras del alfabeto, incluyendo la ñ si asi se indica
 alfabeto = [chr(i) for i in range(97, 123)]
@@ -12,18 +23,30 @@ desplazamientos = input("¿Quieres hacer todos los posibles desplazamientos? si/
 
 # Si quieres realizar todos los desplazamientos, se recorre un bucle para cada desplazamiento y se va imprimiendo cada palabra
 if desplazamientos == "si":
-    palabras = ""
+    palabras_diccionario = []
+
+    def check_word_in_rae(word, id):
+        response = dle.search_by_word(word=word).to_dict()
+        if response["title"] != "Diccionario de la lengua española | Edición del Tricentenario | RAE - ASALE":
+            print("SI, puede ser nuestra solución")
+            palabras_diccionario.append([word,id])
+        else:
+            print("NO, no puede ser nuestra solución")
+
     for i in range(1, 27):
+        palabra_descifrada = ""
         for letra in palabra:
             if letra in alfabeto:
                 indice = alfabeto.index(letra)
                 nuevo_indice = (indice + i) % len(alfabeto)
-                palabras += alfabeto[nuevo_indice] 
+                palabra_descifrada += alfabeto[nuevo_indice]
             else:
-                palabras += letra
-        print(f"{i}. {palabras}")
-        time.sleep(0.25)
-        palabras = ""
+                palabra_descifrada += letra
+        print(f"{i}. {palabra_descifrada}")
+        check_word_in_rae(palabra_descifrada, i)
+
+    for palabra in palabras_diccionario:
+        print(f"\n{palabra[0]} es posiblemente la respuesta con {palabra[1]} rotaciones")
     
 # Si quieres un desplazamiento especifico, se pide un numero y se imprime la palabra resultante con ese desplazamiento
 if desplazamientos =="no":
